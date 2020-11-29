435e679f02ec53e5a8ebe32cc6aa115c522cf090
Divya Dhar <divya.dhar@gmail.com>
2020-11-28 23:01:58 -0800

    completed app

--- guestbook-frontend/src/App.js
+++ guestbook-frontend/src/App.js
@@ -2,32 +2,72 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import React from "react";
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button';
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
 
 // import 'fontsource-roboto';
 // <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
 function App() {
-  const [guestbook, setGuestbook] = React.useState([
-    { name: "Jack", message: "This is a cool app" },
-    {
-      name: "Divya",
-      message: "I like it too",
-    },
-  ]);
+  const endpoint =
+    "https://us-central1-divya-guestbook.cloudfunctions.net/guestbookApi";
+
+  // const [guestbook, setGuestbook] = React.useState([
+  //   { name: "Jack", message: "This is a cool app" },
+  //   {
+  //     name: "Divya",
+  //     message: "I like it too",
+  //   },
+  // ]);
+
+  const [guestbook, setGuestbook] = React.useState([]);
 
   //Add new guest message
   const [name, setName] = React.useState("");
   const [message, setMessage] = React.useState("");
 
-  //Post a Message
-  const postMessage = () => {
-    setGuestbook([...guestbook, { name: name, message: message }]);
+  //Get guestbook data
+  const getGuestbook = () => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((data) => setGuestbook(data));
   };
+
+  //Post a Message
+  // const postMessage = () => {
+  //   setGuestbook([...guestbook, { name: name, message: message }]);
+  // };
+
+  //Post Guestbook data
+  const postMessage = () => {
+    let guest = {
+      name: name,
+      message: message,
+    };
+    fetch(endpoint, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+
+      body: JSON.stringify(guest),
+    }).then(() => {
+      getGuestbook();
+    });
+  };
+
+  //on page load
+  React.useEffect(() => {
+    getGuestbook();
+  }, []);
+
 
   return (
     <div className="App">
